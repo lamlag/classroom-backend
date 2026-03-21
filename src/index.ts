4746273f333c46f077ce4e651bf969d971ab42ae
lamlag <lamlagg@gmail.com>
2026-03-21 17:15:20 +0200

    Added apm with site247

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+import AgentAPI from "apminsight";
+
+AgentAPI.config();
 import express, {Request, Response} from 'express';
 import cors from 'cors';
 import subjectRouter from './routes/subjects';
